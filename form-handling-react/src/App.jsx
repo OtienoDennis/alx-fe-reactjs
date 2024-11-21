639b19994fd64be23bd22b615bad1198460bc5ef
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-11-21 22:48:20 +0300

    Create FormikForm component

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,9 +1,10 @@
-import RegistrationForm from './components/RegistrationForm'
+import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/FormikForm'
 
 function App() {
   return (
     <>
-      <RegistrationForm />
+      <FormikForm />
     </>
   )
 }
