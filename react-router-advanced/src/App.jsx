1be50534fed6c9435534e3a0c16e2f4c6e997dfd
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-11-22 22:39:08 +0300

    Updates

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,29 @@
+import {createBrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Login from './components/Login';
+import BlogPost from './components/BlogPost';
+import ProtectedRoute from './components/ProctectedRoute';
+import {useState} from 'react';
+
+function App () {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState( false );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/blog/:postId" element={<BlogPost />} />
+        <Route
+          path="/profile/"
+          element={
+            <ProtectedRoute isAuthenticated={isUserLoggedIn}>
+              <Profile />
+            </ProtectedRoute>} />
+      </Routes>
+   </Router>
+  )
+}
+
+export default App
