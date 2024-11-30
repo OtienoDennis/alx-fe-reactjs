11c64aea9557d17a094f8f1df94c2e822b2360e8
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-11-30 16:40:12 +0300

    Update utility classes to fix responsiveness

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-xs sm:max-w-sm md:max-w-md mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8">
+    <div className="user-profile bg-gray-100 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8">
       <img 
         src="https://via.placeholder.com/150" 
         alt="User" 
-        className='rounded-full w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 mx-auto'/>
-      <h1 className='text-lg sm:text-xl md:text-2xl text-blue-800 my-4'>John Doe</h1>
-      <p className='text-gray-600 text-sm sm:text-base md:text-lg '>Developer at Example Co. Loves to write code and explore new technologies.</p>
+        className='rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto'/>
+      <h1 className='sm:text-lg md:text-xl text-blue-800 my-4'>John Doe</h1>
+      <p className='text-gray-600 sm:text-sm md:text-base'>Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
