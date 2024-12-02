11494467884be7d20adf5b8301b349561ce00419
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-12-02 22:46:05 +0300

    Add styling

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import HomePage from './components/HomePage'
 
 export default function App() {
   return (
-    <div className='underline text-blue-500'>App</div>
+    <>
+      <HomePage />
+    </>
   )
 }
