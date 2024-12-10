fe22a535d29090870798fe611512a1427060692c
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-12-10 22:34:22 +0300

    Changes

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {fetchUserData} from '../services/githubService';
 
 
 export default function Search () {
@@ -7,24 +8,44 @@ export default function Search () {
   const [error, setError] = useState( null );
   const [userData, setUserData] = useState( null );
 
-  function handleSubmit (e) {
+  const handleSubmit  =  async ( e ) => {
     e.preventDefault();
-    if ( userName ) {
-      fetchUserData( userName );
+    setIsLoading( true );
+    setError( null );
+    setUserData( null );
+    
+    try {
+      const data = await fetchUserData( userName );
+      setUserData( data );
+    } catch ( err ) {
+      setError( err.message );
+    } finally {
+      setIsLoading( false );
     }
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="username" className='underline'>Enter UserName: </label>
-      <input
-        className='border border-gray-500 rounded-md p-1 focus:border-blue-500 focus:ring focus:ring-blue-200'
-        placeholder='Enter Username:'
-        type="text"
-        id="username"
-        value={userName}
-        onChange={( e ) => setUserName( e.target.value )}
-      />
-      <button>SUBMIT</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username" className='underline'>Enter UserName: </label>
+        <input
+          className='border border-gray-500 rounded-md p-1 focus:border-blue-500 focus:ring focus:ring-blue-200'
+          placeholder='Enter Username:'
+          type="text"
+          id="username"
+          value={userName}
+          onChange={( e ) => setUserName( e.target.value )}
+          />
+        <button>SUBMIT</button>
+      </form>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>User not Found!</p>}
+      {userData && (
+        <div>
+          <img src={userData.avatar_url} alt={`${userData.login}'s avatar`} />
+          <p>{userData.name}</p>
+          <a href={userData.html_url} target='_blank'>Profile</a>
+        </div>
+      )}
+    </>
   )
 }
---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.github.com/users';
+
+export const fetchUserData = async ( userName ) => {
+    try {
+        const response = await axios.get( `${BASE_URL}/${userName}` );
+        console.log(response.data)
+        return response.data;
+    } catch ( error ) {
+        throw new Error('Looks like we cant find the user')
+    }
+}
